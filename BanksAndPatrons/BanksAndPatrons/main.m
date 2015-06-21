1c6d78c930b4b66e009208f52154c5774ba16aa5
Varindra <varindrahart@gmail.com>
2015-06-21 14:48:36 -0400

    multi accounts

--- BanksAndPatrons/BanksAndPatrons/main.m
+++ BanksAndPatrons/BanksAndPatrons/main.m
@@ -11,6 +11,7 @@
 @interface BankAccount : NSObject
 
 @property (nonatomic) int balance;
+@property (nonatomic) int acctNumber;
 - (void) printBalance;
 @end
 
@@ -25,33 +26,70 @@
 @interface Patron : NSObject
 
 @property (nonatomic) int pocket;
+@property (nonatomic) NSMutableArray * bankAccounts;
 
 - (void)withdraw: (BankAccount*)account :(int)amount;
 - (void)deposit: (BankAccount *)account :(int)amount;
 - (void)printPocket;
+- (NSMutableArray *) myArray;
+
 @end
 
 @implementation Patron
 
-- (void)withdraw: (BankAccount*)account :(int)amount{
-    if( amount <= account.balance){
-        self.pocket += amount;
-        account.balance -= amount;
+-(NSMutableArray*) bankAccounts{
+    if(!_bankAccounts){
+        _bankAccounts= [[NSMutableArray alloc] init];
+        
     }
-    else{
-        NSLog(@"Not enough funds, no withdrawal\n");
+    
+    return _bankAccounts;
+}
+
+
+- (void)withdraw: (BankAccount*)account :(int)amount{
+    int checker = 0;
+    for (BankAccount *i in self.bankAccounts){
+        if( i.acctNumber == account.acctNumber){
+            checker=1;
+        }
+    }
+    
+    if(checker==1){
+        if( amount <= account.balance){
+            self.pocket += amount;
+            account.balance -= amount;
+        }
+        else{
+            NSLog(@"Not enough funds, no withdrawal\n");
+        }
+    }
+    
+    if(checker==0){
+        NSLog(@"That is not your account. Access denied\n");
     }
 }
 
 - (void)deposit: (BankAccount *)account :(int)amount{
-    
-    if(amount<=self.pocket){
-       account.balance += amount;
-       self.pocket -= amount;
+    int checker = 0;
+    for (BankAccount *i in self.bankAccounts){
+        if( i.acctNumber == account.acctNumber){
+            checker=1;
+        }
     }
     
-    else{
-        NSLog(@"Cannot deposit more than: %d\n", self.pocket);
+    if(checker==1){
+        if( amount < self.pocket){
+            self.pocket -= amount;
+            account.balance += amount;
+        }
+        else{
+            NSLog(@"Not enough funds, no withdrawal\n");
+        }
+    }
+    
+    if(checker==0){
+        NSLog(@"That is not your account. Access denied\n");
     }
 }
 
@@ -68,6 +106,23 @@ int main(int argc, const char * argv[]) {
     
         BankAccount *acct1 =[[BankAccount alloc] init];
         acct1.balance = 1000;
+        acct1.acctNumber = 111;
+        
+        BankAccount *acct2 =[[BankAccount alloc] init];
+        acct2.balance = 800;
+        acct2.acctNumber = 222;
+        
+        BankAccount *acct3 =[[BankAccount alloc] init];
+        acct3.balance = 100;
+        acct3.acctNumber = 333;
+        
+        BankAccount *acct4 =[[BankAccount alloc] init];
+        acct4.balance = 500;
+        acct4.acctNumber = 444;
+        
+        BankAccount *acct5 =[[BankAccount alloc] init];
+        acct5.balance = 10000;
+        acct5.acctNumber = 555;
         
         Patron  *a = [[Patron alloc] init];
         Patron  *b = [[Patron alloc] init];
@@ -76,15 +131,45 @@ int main(int argc, const char * argv[]) {
         a.pocket = 200;
         b.pocket = 150;
         c.pocket = 500;
-    
+        
+        [a.bankAccounts addObject:acct1];
+        
+        [b.bankAccounts addObject:acct4];
+        [b.bankAccounts addObject:acct2];
+        [b.bankAccounts addObject:acct3];
+        
+        [c.bankAccounts addObject:acct1];
+        [c.bankAccounts addObject:acct4];
+        [c.bankAccounts addObject:acct5];
+        
+        
+        NSLog(@"Person a: \n");
         [a deposit:acct1 :250];
+        [a withdraw:acct4 :50];
+        [a printPocket];
+        [acct1 printBalance];
+        [acct4 printBalance];
+        
+        NSLog(@"Person b: \n");
+        [b deposit:acct4 :5];
+        [b printPocket];
+        [acct4 printBalance];
+        
+    
+        [c withdraw:acct4 :5];
         [c printPocket];
         [acct1 printBalance];
     
     
     
     
-    
+        for (int i = 0; i < a.bankAccounts.count; i++) {
+            NSLog(@"%@", [a.bankAccounts objectAtIndex:i]);
+        }
+        
+        for (BankAccount *account in a.bankAccounts) {
+            NSLog(@"%@", account);
+        }
     
     
     
